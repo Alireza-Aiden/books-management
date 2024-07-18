25879546d02293b9eed67267314e896931298257
alireza abedi <a.alireza1347@gmail.com>
2024-07-18 14:07:47 +0330

    Update Plugin Main File

--- books-management.php
+++ books-management.php
@@ -1,26 +1,17 @@
 <?php
 /**
  * Plugin Name: Books Management
- * Plugin URI:  https://yourwebsite.com
  * Description: A plugin for managing books in WordPress
  * Version:     1.0.0
- * Author:      Ali
- * Author:      Ali
- * Author URI:  https://yourwebsite.com
+ * Author:      Alireza Abedi   
+ * Author URI:  https://aidenweb.ir
  * Text Domain: books-management
  * Domain Path: /languages
  */
  
 
-// if (!defined('ABSPATH')) {
-//     exit;
-// }
 require __DIR__ . '/vendor/autoload.php';
 
-// // Autoload Rabbit and Composer dependencies
-// if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-//     require_once __DIR__ . '/vendor/autoload.php';
-// }
 use BooksManagement\BooksManagementPlugin;
 function books_management_init() {
     $plugin = new BooksManagementPlugin(__DIR__, __FILE__);
